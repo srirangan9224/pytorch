b014479d5597a395f9a026f939e34f95cd0b042a
Srirangan Sudarshan <srirangan@Srirangans-MacBook-Pro.local>
2025-06-01 19:45:59 -0400

    installed cmake, wrote cpp code

---
+++ build/CMakeFiles/pytorch.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pytorch.dir/transfer.cpp.o"
+  "CMakeFiles/pytorch.dir/transfer.cpp.o.d"
+  "pytorch"
+  "pytorch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pytorch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
